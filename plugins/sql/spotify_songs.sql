8ea30de86c8221ae51b64c6dabce4834e1d22ef2
calbergs <60953643+calbergs@users.noreply.github.com>
2022-12-22 18:57:21 -0600

    adding sql

---
+++ plugins/sql/spotify_songs.sql
@@ -0,0 +1,17 @@
+SELECT
+	count(played_at_utc) over () as total_records,
+	last_updated_datetime_utc at time zone 'utc' at time zone 'cst' as last_updated_datetime_cst,
+	played_at_utc at time zone 'utc' at time zone 'cst' as played_at_cst,
+	song_name,
+	artist_name,
+	song_duration_ms,
+	song_link,
+	album_art_link,
+	album_name,
+	album_release_date,
+	album_id,
+	artist_id
+FROM
+    public.spotify_songs
+ORDER BY
+    played_at_utc DESC;
