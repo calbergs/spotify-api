7dd0dce8dad75f84f3bd563923258eba2dce32bb
calbergs <60953643+calbergs@users.noreply.github.com>
2022-12-30 20:22:39 -0600

    adding ConnectPostgres class

--- operators/postgres_connect.py
+++ operators/postgres_connect.py
@@ -1,15 +1,17 @@
 import psycopg2
 from secrets import pg_user, pg_password, host, port, dbname
 
-def connect_to_postgres():
-    conn = psycopg2.connect(f"host='{host}' port='{port}' dbname='{dbname}' user='{pg_user}' password='{pg_password}'")
-    cur = conn.cursor()
-    query ="""
-    copy spotify_genres
-    from '/opt/airflow/dags/spotify_data/spotify_genres.csv'
-    delimiter ',' csv;
-    """
-    cur.execute(query)
+class ConnectPostgres:
+    def __init__(self):
+        self.host = host
+        self.port = port
+        self.dbname = dbname
+        self.pg_user = pg_user
+        self.pg_password = pg_password
+
+    def postgres_connector(self):
+        conn = psycopg2.connect(f"host='{self.host}' port='{self.port}' dbname='{self.dbname}' user='{self.pg_user}' password='{self.pg_password}'")
+        return conn
 
 if __name__ == "__main__":
-    connect_to_postgres()
+    conn = postgres_connector()
