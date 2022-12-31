f045d6ada9a1c6779b9a0f392a6d063e0e969270
calbergs <60953643+calbergs@users.noreply.github.com>
2022-12-30 21:04:52 -0600

    reformatting

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -6,10 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
-from airflow_dbt.operators.dbt_operator import (
-    DbtRunOperator,
-    DbtTestOperator
-)
+from airflow_dbt.operators.dbt_operator import DbtRunOperator, DbtTestOperator
 from datetime import timedelta, datetime
 
 def task_fail_slack_alert(context):
