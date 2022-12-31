9ef971ef4c35832b3e2af4acef318e066953a3bc
calbergs <60953643+calbergs@users.noreply.github.com>
2022-12-30 20:23:05 -0600

    cleaning up script

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -41,69 +41,6 @@ def task_fail_slack_alert(context):
     )
     return failed_alert.execute(context=context)
 
-def task_success_slack_alert(context):
-    slack_webhook_token = BaseHook.get_connection(slack_conn_id).password
-    slack_msg = """
-        :white_check_mark: Task Succeeded
-        *Task*: {task}
-        *Dag*: {dag}
-        *Execution Time*: {exec_date}
-        *Log URL*: {log_url}
-        """.format(
-        task=context.get('task_instance').task_id,
-        dag=context.get('task_instance').dag_id,
-        ti=context.get('task_instance'),
-        exec_date=context.get('execution_date'),
-        log_url=context.get('task_instance').log_url,
-    )
-    failed_alert = SlackWebhookOperator(
-        task_id='slack_test',
-        http_conn_id='slack',
-        webhook_token=slack_webhook_token,
-        message=slack_msg,
-        username='airflow',
-        dag=dag
-    )
-    return failed_alert.execute(context=context)
-
-args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 12, 21),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
-    'on_success_callback': None,
-    'on_failure_callback': task_fail_slack_alert
-}
-
-dag = DAG(
-    dag_id='spotify_dag',
-    schedule_interval='*/30 0-6,14-23 * * *',
-    max_active_runs=1,
-    catchup=False,
-    default_args=args
-)
-
-extract_spotify_data = BashOperator(
-    task_id='extract_spotify_data',
-    bash_command='python3 /opt/airflow/operators/main.py',
-    dag=dag
-)
-
-create_if_not_exists_spotify_genres_table = PostgresOperator(
-    task_id="create_if_not_exists_spotify_genres_table",
-    postgres_conn_id="postgres_localhost",
-    sql="sql/create_spotify_genres.sql",
-    dag=dag
-)
-
-create_if_not_exists_spotify_songs_table = PostgresOperator(
-    task_id="create_if_not_exists_spotify_songs_table",
-    postgres_conn_id="postgres_localhost",
-    sql="sql/create_spotify_songs.sql",
-    dag=dag
-)
-
 def copy_expert_genres_csv():
     hook = PostgresHook(
         postgres_conn_id='postgres_localhost',
@@ -134,40 +71,77 @@ def copy_expert_songs_csv():
                         '/opt/airflow/dags/spotify_data/spotify_songs.csv')
             connection.commit()
 
-load_genres = PythonOperator(
-    task_id="load_genres",
-    python_callable=copy_expert_genres_csv,
-    dag=dag
-)
+args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 12, 21),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'on_success_callback': None,
+    'on_failure_callback': task_fail_slack_alert
+}
 
-load_songs = PythonOperator(
-    task_id="load_songs",
-    python_callable=copy_expert_songs_csv,
-    dag=dag
-)
+with DAG(
+    dag_id='spotify_dag',
+    schedule_interval='*/30 0-6,14-23 * * *',
+    max_active_runs=1,
+    catchup=False,
+    default_args=args
+) as dag:
 
-dbt_run = DbtRunOperator(
-    task_id="dbt_run",
-    dir="/opt/airflow/operators/dbt/",
-    profiles_dir="/opt/airflow/operators/dbt/",
-    dag=dag
-)
+    extract_spotify_data = BashOperator(
+        task_id='extract_spotify_data',
+        bash_command='python3 /opt/airflow/operators/main.py'
+    )
 
-dbt_test = DbtTestOperator(
-    task_id="dbt_test",
-    dir="/opt/airflow/operators/dbt/",
-    profiles_dir="/opt/airflow/operators/dbt/",
-    dag=dag
-)
+    create_if_not_exists_spotify_genres_table = PostgresOperator(
+        task_id="create_if_not_exists_spotify_genres_table",
+        postgres_conn_id="postgres_localhost",
+        sql="sql/create_spotify_genres.sql"
+    )
 
-start_task = DummyOperator(
-    task_id="start",
-    dag=dag
-)
+    create_if_not_exists_spotify_songs_table = PostgresOperator(
+        task_id="create_if_not_exists_spotify_songs_table",
+        postgres_conn_id="postgres_localhost",
+        sql="sql/create_spotify_songs.sql"
+    )
 
-end_task = DummyOperator(
-    task_id="end",
-    dag=dag
-)
+    load_genres = PythonOperator(
+        task_id="load_genres",
+        python_callable=copy_expert_genres_csv
+    )
 
-start_task >> [create_if_not_exists_spotify_genres_table, create_if_not_exists_spotify_songs_table] >> extract_spotify_data >> [load_genres, load_songs] >> dbt_run >> dbt_test >> end_task
+    load_songs = PythonOperator(
+        task_id="load_songs",
+        python_callable=copy_expert_songs_csv
+    )
+
+    dbt_run = DbtRunOperator(
+        task_id="dbt_run",
+        dir="/opt/airflow/operators/dbt/",
+        profiles_dir="/opt/airflow/operators/dbt/"
+    )
+
+    dbt_test = DbtTestOperator(
+        task_id="dbt_test",
+        dir="/opt/airflow/operators/dbt/",
+        profiles_dir="/opt/airflow/operators/dbt/"
+    )
+
+    start_task = DummyOperator(
+        task_id="start"
+    )
+
+    end_task = DummyOperator(
+        task_id="end"
+    )
+
+    (
+    start_task
+    >> [create_if_not_exists_spotify_genres_table, create_if_not_exists_spotify_songs_table]
+    >> extract_spotify_data
+    >> [load_genres, load_songs]
+    >> dbt_run
+    >> dbt_test
+    >> end_task
+    )
