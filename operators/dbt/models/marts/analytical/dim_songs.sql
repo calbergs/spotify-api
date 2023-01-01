9d8698b9b74a1889d2a11e86e41ea31c45805611
calbergs <60953643+calbergs@users.noreply.github.com>
2023-01-01 09:40:10 -0600

    updating date to datetime

--- operators/dbt/models/marts/analytical/dim_songs.sql
+++ operators/dbt/models/marts/analytical/dim_songs.sql
@@ -8,7 +8,7 @@ with songs as (
 
 	select
 		played_at_utc at time zone 'utc' at time zone 'cst' as played_at_cst,
-		cast(played_date_utc at time zone 'utc' at time zone 'cst' as date) as played_date_cst,
+		cast(played_at_utc at time zone 'utc' at time zone 'cst' as date) as played_date_cst,
 		song_name,
 		artist_name,
 		song_duration_ms,
