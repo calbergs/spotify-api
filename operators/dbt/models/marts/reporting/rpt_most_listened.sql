aa51219f214d38fd15a8a946f57c692f8d9c7c9a
calbergs <albert.rich.cheng@gmail.com>
2026-06-07 10:41:13 -0500

    updating logic

--- operators/dbt/models/marts/reporting/rpt_most_listened.sql
+++ operators/dbt/models/marts/reporting/rpt_most_listened.sql
@@ -4,14 +4,47 @@
     )
 }}
 
-select distinct
-    song_name,
-    artist_name,
-    album_name,
-    artist_genre,
-    song_link,
-    count(track_id) over (partition by artist_name, song_name) as times_song_listened,
-    count(artist_id) over (partition by artist_id) as times_artist_listened,
-    max(played_date) over (partition by track_id) as song_last_listened_date
+with listening as (
 
-from {{ ref('fct_listening_activity') }}
+    select * from {{ ref('fct_listening_activity') }}
+
+),
+
+song_stats as (
+
+    select
+        song_name,
+        artist_name,
+        song_link,
+        min(album_name) as album_name,
+        min(artist_genre) as artist_genre,
+        count(*) as times_song_listened,
+        max(played_date) as song_last_listened_date,
+        max(artist_id) as artist_id
+    from listening
+    group by song_name, artist_name, song_link
+
+),
+
+artist_stats as (
+
+    select
+        artist_id,
+        count(*) as times_artist_listened
+    from listening
+    group by artist_id
+
+)
+
+select
+    s.song_name,
+    s.artist_name,
+    s.album_name,
+    s.artist_genre,
+    s.song_link,
+    s.times_song_listened,
+    a.times_artist_listened,
+    s.song_last_listened_date
+from song_stats s
+join artist_stats a
+    on s.artist_id = a.artist_id
